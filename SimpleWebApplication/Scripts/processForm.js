7aae8657ebf0a27b73ad4ef1e924e20d49e69e5d
Mark Chouanard <mac@chouanard.com>
2012-08-27 09:35:19 -0500

    Initialize Repository

---
+++ SimpleWebApplication/Scripts/processForm.js
@@ -0,0 +1,57 @@
+﻿
+$(document).ready(function () {
+    $('#contact-form').validate({
+        rules: {
+            name: {
+                minlength: 3,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                minlength: 3,
+                required: true
+            },
+        },
+        highlight: function (label) {
+            $(label).closest('.control-group').addClass('error');
+        },
+        success: function (label) {
+            label
+				.text('OK!').addClass('valid')
+				.closest('.control-group').addClass('success');
+        }
+    });
+
+});
+
+$("#cancel").click(function () {
+    $("label.error").hide();
+    $(".error").removeClass("error");
+});
+
+$("#submit").click(function () {
+
+    if ($("#contact-form").valid()) {
+        var formData = $('#contact-form').serialize();
+        $.support.cors = true;
+        $.ajax({
+            url: "http://localhost:51817/api/Contactform/Post",
+            type: "POST",
+            crossDomain: true,
+            data: formData,
+            dataType: "html",
+            success: function (result) {
+                $('#contactForm').html(result);
+            },
+            error: function (jqXHR, tranStatus, errorThrown) {
+                $('#contactForm').html("<h3>POST Error!</h3>Browser may not support <a href='http://en.wikipedia.org/wiki/Cross-origin_resource_sharing#Browser_support'>Cross Origin Resource Sharing</a> ");
+            }
+        });
+    }
+});
+
+
+
---
+++ WebAPIApp/Controllers/ContactFormController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using WebAPIApp.Models;
+
+namespace WebAPIApp.Controllers
+{
+    public class ContactFormController : ApiController
+    {
+        // GET /api/contactform
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET /api/contactform/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST /api/// POST /api/contactform
+        [HttpPost]
+        public HttpResponseMessage Post(Contact contact)
+        {
+            // Process the Form data
+            // contact.Name, contact.Email and contact.Message
+
+            // Send the client an HTML message indicating the status of processing the Form
+            string responseMessageHTML = ("<h3>Thank You!</h3>Thanks for contacting XYZ Company. Someone from our marketing team will be contacting " + contact.Name + ", at " + contact.Email);
+            
+            // Build an HTTPResponseMessage and return to client
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            responseMessage.Content = new StringContent(responseMessageHTML);
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return responseMessage;
+        }
+
+        // PUT /api/contactform/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/contactform/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
